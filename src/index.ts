188cdc112ce1b4921254d2a65abf43353f1fcb18
samrat <samratdot@outlook.com>
2025-01-15 23:32:28 +0530

    basic express

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-console.log(100)
+import express from 'express'
+
+const app = express()
+const port = process.env.PORT || 7000
+
+app.get('/', (request, response) => {
+    response.send("<h1>Hello World</h1>")
+})
+app.get('/login', (request, response) => {
+    response.send("<p>login page</p>")
+})
+app.listen(port, () => {
+    console.log(`app is runing on port http://localhost:${port}`)
+})
