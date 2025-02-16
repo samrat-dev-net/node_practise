91499702cad4d2c1c3d243dc69dfc5b5967efe2d
samrat <samratdot@outlook.com>
2025-02-16 15:00:46 +0530

    .

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,22 +1,10 @@
 import { generateAuthLink } from "@/controllers/auth";
-import { NextFunction, Request, Response, Router } from "express";
-import { z } from 'zod'
+import { emailValidationSchema, validate } from "@/middlewares/validator";
+import { NextFunction, Request, RequestHandler, Response, Router } from "express";
 
 const authRouter = Router()
-const schema = z.object({
-    email: z.string({
-        required_error: 'email is missing!'
-    }).email('Invalid email address')
-})
-authRouter.post('/generate-link', (req: Request, res: Response, next: NextFunction): any => {
-    const result = schema.safeParse(req.body)
-    if (!result.success) {
-        console.log(JSON.stringify(result, null, 2));
-        const err = result.error.flatten().fieldErrors
-        return res.status(422).json({ err })
-    }
-    next();
-}, generateAuthLink)
+
+authRouter.post('/generate-link', validate(emailValidationSchema), generateAuthLink)
 
 export default authRouter
 
