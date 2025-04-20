4bb8db774d74bc42d5b8f6801bcc1a9344b404a2
samratweb <samratdot@outlook.com>
2025-04-20 15:29:34 +0530

    .

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -4,14 +4,9 @@ import nodemailer from 'nodemailer'
 import { hashSync, compareSync } from "bcrypt";
 import VerificationTokenModel from '@/models/verificationToken';
 import UserModel from "@/models/user";
+import mail from "@/utils/mail";
 
 export const generateAuthLink: RequestHandler = async (req, res) => {
-    /* 1. Generate Unique token for every users
-    2. Store that token securely inside the database
-    so that we can validate it in future.
-    3. Create a link which include that sec0ure token and user information
-    4. Send that link to users email address.
-    5. Notify user to look inside the email to get the login link*/
     const { email } = req.body
     let user = await UserModel.findOne({ email })
     if (!user) {
@@ -27,24 +22,12 @@ export const generateAuthLink: RequestHandler = async (req, res) => {
         userId,
         token: randomToken,
     })
-
-    const transport = nodemailer.createTransport({
-        host: "sandbox.smtp.mailtrap.io",
-        port: 2525,
-        auth: {
-            user: "d868aafb9d2cf2",
-            pass: "675c1a5d1b4d61"
-        }
-    });
-    const link = `http://localhost:3000/verify?token=${randomToken}&userId=${userId}`
-    await transport.sendMail({
-        to: user.email,
-        from: 'verification@myapp.com',
-        subject: 'Auth verification',
-        html: `<div>
-                <p>Please click on <a href="${link}">this link</a> to verify your account.</p>
-            </div>`
+    const link = `${process.env.VERIFICATION_LINK}?token=${randomToken}&userId=${userId}`
+    await mail.sendVerificationMail({
+        link,
+        to: user.email
     })
+
     res.json({
         message: 'Please check your email for link'
     });
